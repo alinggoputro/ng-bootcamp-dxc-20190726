3f4c389974394799fbee95a7846e1d54412ff26e
OXYGEN\linggopa <alinggoputro@dxc.com>
2019-07-26 11:32:54 +1000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mes-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-ecommerce-site';
+}
